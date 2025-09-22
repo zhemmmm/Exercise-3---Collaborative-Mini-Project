4d702168b9dd2bbab51c3bbbd414a53f0aa57242
DanielBanaag <dbanaag6@gmail.com>
2025-09-22 17:48:05 +0800

    fix: add input validation and safe task removal; improve menu UX

--- toDoApp.py
+++ toDoApp.py
@@ -1,21 +1,25 @@
 # toDoApp.py
 
-tasks=[]
+tasks = []
 
-def addtask(task) :
-  tasks.append(task)
-  print("task added!")
 
-def showTasks( ):
-    if len(tasks)==0 :
-      print("no tasks yet")
+def addtask(task):
+    tasks.append(task)
+    print("task added!")
+
+
+def showTasks():
+    if len(tasks) == 0:
+        print("no tasks yet")
     else:
-     for i in range (len(tasks)):
-      print(i+1,".",tasks[i])
+        for i in range(len(tasks)):
+            print(i + 1, ".", tasks[i])
+
 
 def removetask(tasknumber):
-    tasks.pop(tasknumber) 
+    tasks.pop(tasknumber)
     print("task removed!!")
+
 
 def main():
     while True:
@@ -24,16 +28,18 @@ def main():
         print("3.Remove Task")
         print("4- Exit")
         ch = input("enter choice : ")
-        if ch=="1":
+        if ch == "1":
             t = input("enter task : ")
             addtask(t)
-        elif ch=="2":
+        elif ch == "2":
             showTasks()
-        elif ch=="3":
-            n=int(input("enter task no to remove: "))
-            removetask(n)   
-        elif ch=="4":
-            break;
+        elif ch == "3":
+            n = int(input("enter task no to remove: "))
+            removetask(n)
+        elif ch == "4":
+            break
         else:
             print("wrong choice!!")
+
+
 main()
