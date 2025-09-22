9c90062cb53e8d2ac3914db86ad22042b9e77cd9
zhemmmm <zhemierpadora@gmail.com>
2025-09-22 18:07:15 +0800

    fix: updated for input

--- toDoApp.py
+++ toDoApp.py
@@ -1,39 +1,56 @@
 # toDoApp.py
 
-tasks=[]
+tasks = []
 
-def addtask(task) :
-  tasks.append(task)
-  print("task added!")
+def addtask(task):
+    tasks.append(task)
+    print("Task added!")
 
-def showTasks( ):
-    if len(tasks)==0 :
-      print("no tasks yet")
+def showTasks():
+    if len(tasks) == 0:
+        print("No tasks yet.")
     else:
-     for i in range (len(tasks)):
-      print(i+1,".",tasks[i])
+        for i in range(len(tasks)):
+            print(i + 1, ".", tasks[i])
 
 def removetask(tasknumber):
-    tasks.pop(tasknumber) 
-    print("task removed!!")
+    if 1 <= tasknumber <= len(tasks):   # validate input
+        tasks.pop(tasknumber - 1)       # adjust for 0-based index
+        print("Task removed!")
+    else:
+        print("Invalid task number!")
 
 def main():
     while True:
-        print("1 Add Task")
-        print("2.Show Tasks")
-        print("3.Remove Task")
-        print("4- Exit")
-        ch = input("enter choice : ")
-        if ch=="1":
-            t = input("enter task : ")
-            addtask(t)
-        elif ch=="2":
+        print("\n--- To-Do App ---")
+        print("1. Add Task")
+        print("2. Show Tasks")
+        print("3. Remove Task")
+        print("4. Exit")
+        
+        ch = input("Enter choice: ").strip()
+        
+        if ch == "1":
+            t = input("Enter task: ").strip()
+            if t:
+                addtask(t)
+            else:
+                print("Task cannot be empty!")
+        elif ch == "2":
             showTasks()
-        elif ch=="3":
-            n=int(input("enter task no to remove: "))
-            removetask(n)   
-        elif ch=="4":
-            break;
+        elif ch == "3":
+            if len(tasks) == 0:
+                print("No tasks to remove.")
+            else:
+                try:
+                    n = int(input("Enter task no. to remove: "))
+                    removetask(n)
+                except ValueError:
+                    print("Please enter a valid number!")
+        elif ch == "4":
+            print("Exiting... Goodbye!")
+            break
         else:
-            print("wrong choice!!")
+            print("Wrong choice!")
+
 main()
